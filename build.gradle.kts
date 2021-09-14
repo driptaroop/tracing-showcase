f13e55837c4bd821db52b118bb086b5127981c86
Driptaroop Das <driptaroop.das@dkbcodefactory.com>
2021-09-14 17:35:07 +0200

    opentracing jaeger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins{
     id("org.springframework.boot") version "2.5.4" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
@@ -13,4 +15,54 @@ allprojects {
 
     group = "org.dripto"
     version = "0.0.1-SNAPSHOT"
+}
+
+subprojects {
+    apply {
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+    }
+
+    val implementation by configurations
+    val testImplementation by configurations
+    val runtimeOnly by configurations
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("io.opentracing.contrib:opentracing-spring-jaeger-cloud-starter:3.3.1")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+            languageVersion = "1.5"
+        }
+    }
+
+    if(name.endsWith("name-service")) {
+        apply {
+            plugin("org.jetbrains.kotlin.plugin.jpa")
+        }
+
+        dependencies {
+            implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+            implementation("io.opentracing.contrib:opentracing-jdbc:0.2.15")
+            runtimeOnly("com.h2database:h2")
+        }
+    }
 }
